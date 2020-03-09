b12a810655e73a59e69e38cd78a7d9e837adf1b7
Amitchy <38037388+Amitchy@users.noreply.github.com>
2020-03-09 15:25:15 +0530

    Add files via upload

---
+++ Group 36- Waste_Segregation/Waste_Segregation2.ino
@@ -0,0 +1,123 @@
+
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+int trigPin = 15;
+int echoPin = 13;
+long duration;
+int distance;
+// WiFi parameters
+#define WLAN_SSID       "DT_LAB"
+#define WLAN_PASS       "fthu@050318"
+
+// Adafruit IO
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883
+#define AIO_USERNAME    "AmitChoudhary"
+#define AIO_KEY         "9b45b727bb9648e69a7ebfa85c09c1ed"  // Obtained from account info on io.adafruit.com
+
+WiFiClient client;
+ 
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+ 
+/****************************** Feeds ***************************************/
+ 
+Adafruit_MQTT_Publish dustbin = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/dustbin");
+
+unsigned long currentMillis = 0;
+int interval = 10000;
+
+/*************************** Sketch Code ************************************/
+
+void setup() {
+
+ pinMode(trigPin, OUTPUT); 
+   pinMode(echoPin, INPUT);
+  
+  Serial.begin(115200);
+  Serial.println(F("Adafruit IO Example"));
+
+  // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  delay(10);
+  Serial.print(F("Connecting to "));
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(F("."));
+  }
+  Serial.println();
+
+  Serial.println(F("WiFi connected"));
+  Serial.println(F("IP address: "));
+  Serial.println(WiFi.localIP());
+
+  // connect to adafruit io
+   mqtt.connect();
+
+}
+
+void loop() {
+  
+  // ping adafruit io a few times to make sure we remain connected
+  if(! mqtt.ping(3)) {
+    // reconnect to adafruit io
+    if(! mqtt.connected())
+      mqtt.connect();
+  }
+
+ digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+// Calculating the distance
+distance= duration*0.034/2;
+// Prints the distance on the Serial Monitor
+Serial.print("Distance: ");
+Serial.println(distance);
+ if (! dustbin.publish(distance))/////
+    Serial.println(F("Failed to publish Door State"));
+   else
+    Serial.println(F("Door State published!"));
+delay(3000);
+  }
+
+
+// connect to adafruit io via MQTT
+void connect() {
+
+  Serial.print(F("Connecting to Adafruit IO... "));
+
+  int8_t ret;
+
+  while ((ret = mqtt.connect()) != 0) {
+
+    switch (ret) {
+      case 1: Serial.println(F("Wrong protocol")); break;
+      case 2: Serial.println(F("ID rejected")); break;
+      case 3: Serial.println(F("Server unavail")); break;
+      case 4: Serial.println(F("Bad user/pass")); break;
+      case 5: Serial.println(F("Not authed")); break;
+      case 6: Serial.println(F("Failed to subscribe")); break;
+      default: Serial.println(F("Connection failed")); break;
+    }
+
+    if(ret >= 0)
+      mqtt.disconnect();
+
+    Serial.println(F("Retrying connection..."));
+    delay(5000);
+
+  }
+
+  Serial.println(F("Adafruit IO Connected!"));
+
+}
