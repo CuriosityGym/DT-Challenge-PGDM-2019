98df8952f3b50d3235b5abc7bb14105f2abea8d5
Amitchy <38037388+Amitchy@users.noreply.github.com>
2020-03-09 18:37:19 +0530

    Add files via upload

---
+++ Group 4- Water level (Flood)/Flood.ino
@@ -0,0 +1,35 @@
+int sensorPin = A0; 
+int sensorPin1 = A1; 
+int sensorValue;  
+int sensorValue1; 
+int limit = 850; 
+
+void setup() {
+ Serial.begin(9600);
+ pinMode(13, OUTPUT);
+ pinMode(12, OUTPUT);
+}
+
+void loop() {
+
+ sensorValue = analogRead(sensorPin);
+ sensorValue1 = analogRead(sensorPin1); 
+ Serial.println("Analog Value : ");
+ Serial.println(sensorValue);
+ Serial.println("Analog Value1 : ");
+ Serial.println(sensorValue1);
+ 
+ if (sensorValue<limit) {
+ digitalWrite(13, HIGH); 
+ }
+ else {
+ digitalWrite(13, LOW); 
+ }
+ if (sensorValue1<limit) {
+ digitalWrite(12, HIGH); 
+ }
+ else {
+ digitalWrite(12, LOW); 
+ }
+ delay(1000); 
+}
